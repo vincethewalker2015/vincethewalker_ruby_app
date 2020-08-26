0bb799c1fc9431815d7bf98d43b01f15c9fe74ee
Vincent <vincethewalker@yahoo.co.uk>
2020-08-26 16:40:06 +0100

    Created example test

--- test.rb
+++ test.rb
@@ -2,20 +2,32 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
-class TestMeme < Minitest::Test
-  def setup
-    @meme = Meme.new
+class TestWebserver < Minitest::Test
+  
+
+  # test "Should see webserver log" do
+  #   skip "Will do this first"
+  # end
+  # def should_see_webserver_log 
+  #   if File.exist?('parser')
+  #     assert_response :success
+  #   end
+  # end
+
+  def test_that_parser_file_exists 
+    if File.exist?('parserbitch')
+    assert true
+    end
   end
 
-  def test_that_kitty_can_eat
-    assert_equal "OHAI!", @meme.i_can_has_cheezburger?
-  end
+  # def test_that_webserverlog_file_exists 
+  #   File.exist?('webserver')
+  #     assert :true
+  # end
 
-  def test_that_it_will_not_blend
-    refute_match /^no/i, @meme.will_it_blend?
-  end
+  # def test_that_will_be_skipped 
+  #   skip "test this later"
+  # end
 
-  def test_that_will_be_skipped
-    skip "test this later"
-  end
 end
+
