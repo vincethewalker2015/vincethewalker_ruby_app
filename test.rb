4094eac58897bde2a41e2c1866305c106228ef43
vincentbwalker <vincethewalker@gmail.com>
2020-08-27 19:52:31 +0100

    Added Initialize method

--- test.rb
+++ test.rb
@@ -3,31 +3,43 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class TestWebserver < Minitest::Test
+
+  def initialize(file_path) #Finds webserver.log
+    @logs = parse(file_path)
+  end
   
-
-  # test "Should see webserver log" do
-  #   skip "Will do this first"
-  # end
-  # def should_see_webserver_log 
-  #   if File.exist?('parser')
-  #     assert_response :success
-  #   end
+  # def test_a_fast_operation
+  #   pass
   # end
 
-  def test_that_parser_file_exists 
-    if File.exist?('parserbitch')
-    assert true
+  # def test_a_slower_operation
+  #   sleep(0.33) && pass
+  # end
+
+  # def test_a_really_slow_operation
+  #   sleep(1) && pass
+  # end
+
+  # def test_pass
+  #   pass
+  # end
+
+  # def test_fail
+  #   pass
+  # end
+
+  private
+
+  def parse(file_path) 
+    default_value = Hash.new { |hash, key| hash[key] = [] }
+
+    File.open(file_path) do |file|
+      file.each_with_object(default_value) do |line, hsh|
+        site, ip = line.split(/\s+/)
+        hsh[site] << ip
+      end
     end
   end
-
-  # def test_that_webserverlog_file_exists 
-  #   File.exist?('webserver')
-  #     assert :true
-  # end
-
-  # def test_that_will_be_skipped 
-  #   skip "test this later"
-  # end
 
 end
 
