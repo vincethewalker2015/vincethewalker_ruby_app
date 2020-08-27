9b1012b0cf793c149ff67c0cbfb5dc86d860ede8
vincentbwalker <vincethewalker@gmail.com>
2020-08-27 16:37:51 +0100

    Created 'most visits' function

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,6 +6,13 @@ class Parser
     @logs = parse(file_path)
   end
 
+  def most_visits
+    @logs
+      .sort_by { |_, ips| ips.length }
+      .reverse
+      .map { |site, ips| pluralize(site, ips.length, 'visit') }
+  end
+
   private
 
   def parse(file_path) 
@@ -18,6 +25,16 @@ class Parser
       end
     end
   end
+
+  def pluralize(site, number, description)
+    description = number > 1 ? "#{description}s" : description
+
+    "#{site} #{number} #{description}"
+  end
 end
 
+parser = Parser.new(ARGV.first)
+
+  puts "\nMost visits \n\n"
+  puts parser.most_visits
 
